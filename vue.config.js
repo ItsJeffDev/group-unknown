3cb1376cae2a14d211aeaee6761ef4f36ee148fb
crazylurk25 <raymarkcarizo325@gmail.com>
2025-12-09 02:06:33 -0600

    build: Exclude index.html from the copy plugin via chainWebpack configuration.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin("copy").tap((options) => {
+      options[0].patterns[0].globOptions.ignore.push("**/index.html");
+      return options;
+    });
+  },
+});
